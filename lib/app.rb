b88f0e01ca0cbc6848cd39a3bef9bbdef65f451f
Hanif Fakira <fakira.work@gmail.com>
2016-03-16 10:21:01 +0000

    Q24 WIP

--- lib/app.rb
+++ lib/app.rb
@@ -8,22 +8,22 @@ get '/' do
 end
 
 post '/names' do
-  $player_1 = Player.new(params[:player_1_name]).name
-  $player_2 = Player.new(params[:player_2_name]).name
+  $player_1 = Player.new(params[:player_1_name])
+  $player_2 = Player.new(params[:player_2_name])
   # session[:player_1_name] = params[:player_1_name]
   # session[:player_2_name] = params[:player_2_name]
   redirect '/play'
 end
 
 get '/play' do
-  @name1 = $player_1 #session[:player_1_name]
-  @name2 = $player_2 #session[:player_2_name]
+  @name1 = $player_1.name #session[:player_1_name]
+  @name2 = $player_2.name #session[:player_2_name]
   erb(:play)
 end
 
 get '/attack' do
-  @name1 = $player_1
-  @name2 = $player_2
+  @name1 = $player_1.name
+  @name2 = $player_2.name
   erb(:attack)
 end
 
