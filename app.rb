75eff7442d15a9aa2f515c1257c7969d72e0418c
Tobenna <tobennandu@gmail.com>
2016-03-16 10:29:28 +0000

    Indentation and prettyness

--- app.rb
+++ app.rb
@@ -2,31 +2,29 @@ require 'sinatra'
 require './lib/player'
 
 class Battle < Sinatra::Base
+  get '/' do
+    erb(:index)
+  end
 
-get '/' do
-  erb(:index)
-end
+  post '/names' do
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    # session[:player_1_name] = params[:player_1_name]
+    # session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
 
-post '/names' do
-  $player_1 = Player.new(params[:player_1_name])
-  $player_2 = Player.new(params[:player_2_name])
-  # session[:player_1_name] = params[:player_1_name]
-  # session[:player_2_name] = params[:player_2_name]
-  redirect '/play'
-end
+  get '/play' do
+    @name1 = $player_1.name # session[:player_1_name]
+    @name2 = $player_2.name # session[:player_2_name]
+    erb(:play)
+  end
 
-get '/play' do
-  @name1 = $player_1.name #session[:player_1_name]
-  @name2 = $player_2.name #session[:player_2_name]
-  erb(:play)
-end
+  get '/attack' do
+    @name1 = $player_1.name
+    @name2 = $player_2.name
+    erb(:attack)
+  end
 
-get '/attack' do
-  @name1 = $player_1.name
-  @name2 = $player_2.name
-  erb(:attack)
-end
-
-  run! if app_file ==$0
-
+  run! if app_file == $PROGRAM_NAME
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,7 @@
 require 'player'
 
 describe Player do
-  subject(:player) {described_class.new('bob')}
+  subject(:player) { described_class.new('bob') }
   it 'returns name' do
     expect(player.name).to eq 'bob'
   end
