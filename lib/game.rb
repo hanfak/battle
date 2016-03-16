81ee24133aef84b679a45a26defe6b154275a488
Hanif Fakira <fakira.work@gmail.com>
2016-03-16 13:59:24 +0000

    Q26 Done

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,13 @@
+require_relative 'player'
+
 class Game
+  attr_reader :player_1, :player_2
+  
+  def initialize(player_1, player_2, player_class = Player)
+    @player_1 = player_class.new player_1
+    @player_2 = player_class.new player_2
+  end
+
   def attack(player)
     player.damage
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,19 @@
 require 'game'
 
-describe Game do 
-  let(:game) { described_class.new }
+describe Game do
+  let(:game) { described_class.new(:player_1, :player_2, player_class) }
   let(:Game) { described_class }
   let(:player) { double :player }
+  let(:player_class) { double :player_class, new: player }
+  describe '#initialize' do
+    it 'has a player one' do
+      expect(game.player_1).to eq player
+    end
+
+    it 'has a player one' do
+      expect(game.player_2).to eq player
+    end
+  end
 
   describe '#attack' do
     it 'attacks another player' do
